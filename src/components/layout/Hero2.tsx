51cf4bd16bd828d93b7f046f53b676eb77df3724
Raphael Malburg <raphael.malburg@gmail.com>
2024-01-13 13:52:52 +0000

    hero and nav version 2, adjust 5

--- src/components/layout/Hero2.tsx
+++ src/components/layout/Hero2.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Hero2 = () => {
   return (
-    <div className="pt-28 lg:pt-20  h-fit">
+    <div className="pt-28 lg:pt-20 ">
       <div className=" md:w-7/12 w-full absolute h-[720px] bg-gray-100  rounded-br-[140px]   -z-20" />
       <header className="grid px-8">
         <div className="md:container mx-auto  grid h-full w-full grid-cols-1  lg:mt-14 md:grid-cols-2">
@@ -16,23 +16,33 @@ const Hero2 = () => {
               <span className=" text-Cooper"> gourmet</span>
             </Title>
             <Description className="text-Black mb-10">Tudo cuidadosamente escolhido para transformar suas refeições em momentos extraordinários</Description>
-            <div className="grid grid-rows-4 sm:grid-rows-1 sm:grid-cols-3 md:gap-10">
-              <div className=" md:my-0 flex flex-col justify-center ">
-                <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">724Kg+</h3>
-                <p className="text-Black font  text-sm">Em Farinha italiana por ano</p>
+            <div className="grid grid-cols-2  h-96 ">
+              {" "}
+              <div className="grid grid-rows-3  justify-center mt-10 h-fit sm:grid-rows-1 sm:grid-cols-3 z-40 md:gap-10 gap-8">
+                <div className=" md:my-0 flex flex-col justify-center ">
+                  <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">724Kg+</h3>
+                  <p className="text-Black font  text-sm">Em Farinha italiana por ano</p>
+                </div>{" "}
+                <div className=" md:my-0 flex flex-col justify-center">
+                  <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">3257 und+</h3>
+                  <p className="text-Black text-sm">De Tortellonis feitos a mão</p>
+                </div>{" "}
+                <div className=" md:my-0 flex flex-col justify-center">
+                  <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">40+</h3>
+                  <p className="text-Black text-sm">Workshops ao ano</p>
+                </div>{" "}
               </div>{" "}
-              <div className=" md:my-0 flex flex-col justify-center">
-                <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">3257 und+</h3>
-                <p className="text-Black text-sm">De Tortellonis feitos a mão</p>
-              </div>{" "}
-              <div className=" md:my-0 flex flex-col justify-center">
-                <h3 className="border-b-2 w-fit font-semibold  mb-1.5 border-Black">40+</h3>
-                <p className="text-Black text-sm">Workshops ao ano</p>
-              </div>{" "}
-              <Button className="mt-10 w-fit ">
-                <Link href="/">Confira nossas receitas</Link>
-              </Button>
-            </div>
+              <Image
+                width={470}
+                height={576}
+                src="/pastanest.png"
+                alt="Pasta nest"
+                className="col-span-1 mt-20  h-full max-h-[30rem]  max-w-[15rem] -translate-y-32 md:max-h-[38rem]  md:max-w-[18rem] lg:-mt-40 lg:max-h-[48rem] lg:max-w-[24rem] lg:translate-y-0"
+              />{" "}
+            </div>{" "}
+            <Button className="mt-10 w-full  mx-auto px-10 ">
+              <Link href="/">Confira nossas receitas</Link>
+            </Button>
           </div>
           {/**inicio mobile */}
           <Image
@@ -42,7 +52,7 @@ const Hero2 = () => {
             alt="Pasta nest"
             className="  hidden lg:block h-full max-h-[30rem]  max-w-[15rem] -translate-y-32 md:max-h-[38rem]  md:max-w-[18rem] lg:-mt-40 lg:max-h-[48rem] lg:max-w-[24rem] lg:translate-y-0"
           />
-          <div className="grid cols-span-1 grid-cols-2 lg:hidden  ">
+          {/**   <div className="grid cols-span-1 grid-cols-2 lg:hidden  ">
             <div className=" grid md:grid-cols-3 md:gap-10">
               <Image
                 width={470}
@@ -52,7 +62,7 @@ const Hero2 = () => {
                 className="col-span-1 my-20  h-full max-h-[30rem]  max-w-[15rem] -translate-y-32 md:max-h-[38rem]  md:max-w-[18rem] lg:-mt-40 lg:max-h-[48rem] lg:max-w-[24rem] lg:translate-y-0"
               />
             </div>
-          </div>
+          </div> */}
         </div>
       </header>
     </div>
